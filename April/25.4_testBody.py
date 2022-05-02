993d2e1384cdb84eac675be19a60cad00f0fda3c
b1smark <marekuzel1@gmail.com>
2022-05-02 11:44:11 +0200

    commit 1

--- April/25.4_testBody.py
+++ April/25.4_testBody.py
@@ -12,7 +12,7 @@ x_bodu, y_bodu = [],[]
 
 for counter,  i in enumerate (r):
     i = i.split()
-    canvas.create_oval (int(i[0]), int(i[1]),int(i[0]) + 5, int(i[1]) + 5)
+    canvas.create_oval (int(i[0])-2.5, int(i[1])-2.5,int(i[0]) + 2.5, int(i[1]) + 2.5)
     canvas.create_text (int(i[0]), int(i[1])-10, text = counter)
     x_bodu.append(int(i[0]))
     y_bodu.append(int(i[1].strip()))
@@ -23,11 +23,11 @@ def kliknutie(e):#pamatat si kolkate kliknutie je a porovnavat ho s takym cislom
     global kliknutiePoradie
     x = e.x
     y = e.y
-    if math.sqrt((x-x_bodu[kliknutiePoradie]**2 + (y-y_bodu[kliknutiePoradie])**2)) < 7:
-        if ciara == True:
-            canvas.create_line ()
-        ciara = True
+    ciara = False
+    if math.sqrt(((x-x_bodu[kliknutiePoradie])**2) + ((y-y_bodu[kliknutiePoradie])**2)) < 7:
+        canvas.create_line (x_bodu[kliknutiePoradie-1], y_bodu[kliknutiePoradie-1],x_bodu[kliknutiePoradie], y_bodu[kliknutiePoradie])
+        kliknutiePoradie += 1
     else:
-        ciara = False
-    kliknutiePoradie += 1
+        print ("nespravny bod")
+canvas.bind ("<Button-1>", kliknutie)
 mainwin.mainloop()
